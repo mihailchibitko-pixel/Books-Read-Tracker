eceacbb2be985b050edb05077abf10a9a4803e23
Михаил Чибитько <m.chibitko@yandex.ru>
2026-06-30 17:01:20 +0300

    Финальная оптимизация: применил генератор списка для инициализации библиотеки

--- models.py
+++ models.py
@@ -1,9 +1,29 @@
 class Book:
     def __init__(self, title, author, rating, date):
-        self.title = title
-        self.author = author
-        self.rating = rating
-        self.date = date
+        self.title = title        # Название книги
+        self.author = author      # Автор книги
+        self.rating = rating      # Оценка книги
+        self.date = date          # Дата прочтения
 
     def __str__(self):
+        """Красивый вывод книги при печати"""
         return f"'{self.title}' — {self.author} (Оценка: {self.rating}, Дата: {self.date})"
+
+    # Метод разбора объекта в простой словарь для JSON-формата
+    def to_dict(self):
+        return {
+            "title": self.title,
+            "author": self.author,
+            "rating": self.rating,
+            "date": self.date
+        }
+
+    # Метод сборки объекта Book из словаря, прочитанного из файла
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            title=data["title"],
+            author=data["author"],
+            rating=data["rating"],
+            date=data["date"]
+        )
